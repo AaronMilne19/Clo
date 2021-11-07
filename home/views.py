376195c0c56ee424f937a5702be2284c007dd009
Aaron Milne <2466574M@student.gla.ac.uk>
2021-11-07 16:24:49 +0000

    Added blank login and signup pages which dont have functionality but link from navbar

--- home/views.py
+++ home/views.py
@@ -5,3 +5,15 @@ def home(request):
     ctx = {}
     
     return render(request, 'home.html', context=ctx)
+
+
+def login(request):
+    ctx = {}
+
+    return render(request, 'login.html', context=ctx)
+
+
+def signup(request):
+    ctx = {}
+
+    return render(request, 'signup.html', context=ctx)
