76013ba85fd8885abc6153460e760f4ad267a7f6
Jan Wieczorek <2095736w@student.gla.ac.uk>
2021-11-09 16:16:00 +0000

    basic models created

--- boom_saloon/settings.py
+++ boom_saloon/settings.py
@@ -64,7 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'boom_saloon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from home.models import Magazine, UserProfile
+
+
+
+
+admin.site.register(Magazine)
+admin.site.register(UserProfile)
