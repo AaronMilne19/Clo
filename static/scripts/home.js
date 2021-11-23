740845bf0bcb689448ee8d7c047b4089665ada00
Aaron Milne <2466574M@student.gla.ac.uk>
2021-11-23 20:42:38 +0000

    Added div to appear when hovering over links

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,5 +1,7 @@
 var titles = document.getElementsByClassName('mag-title');
 var infoBox = document.getElementById('mainbox');
+var issuesList = document.getElementsByClassName('issue-link');
+var issueBox = document.getElementById('IssueBox');
 
 if (screen.width <= 575) {
     infoBox.scrollIntoView(alignToTop = true);
@@ -9,4 +11,12 @@ for (const title of titles) {
     if ("/" + title.id + "/" === window.location.pathname) {
         title.classList.add("yellow");
     }
+}
+
+function showIssueBox() {
+    issueBox.classList.add('show-issue');
+}
+
+function hideIssueBox() {
+    issueBox.classList.remove('show-issue');
 }
