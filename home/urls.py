384f0377f90c0ac4fa70c17b1f9d3119de9eac1a
Aaron Milne <2466574M@student.gla.ac.uk>
2022-01-04 16:20:00 +0000

    Updates to magazine page

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login, name="login"),
     path('signup/', views.signup, name="signup"),
-    path('<int:id>/', views.magazine, name="magazine"),
+    path('<int:id>', views.magazine, name="magazine"),
     path('<int:id>/<slug:slug>/', views.issue, name="issue"),
 ]
