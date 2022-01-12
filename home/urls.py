ab15bd2f33e8d58a1eb1d9b0f54c0fc4bf5fb663
2404574k <2404574k@student.gla.ac.uk>
2022-01-12 21:40:25 +0000

    resolved url issues

--- home/urls.py
+++ home/urls.py
@@ -6,9 +6,8 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('login/', views.login, name="login"),
-    path('signup/', views.signup, name="signup"),
     path('login/', views.user_login, name="login"),
+    path('myprofile/', views.my_profile, name="myprofile"),
     path('signup/', views.user_signup, name="signup"),
     path('<int:id>', views.magazine, name="magazine"),
     path('<int:id>/<slug:slug>/', views.issue, name="issue"),
