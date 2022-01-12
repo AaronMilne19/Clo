c40c58db8313e9ec3852bb25d122ed6b3d456362
2404574k <2404574k@student.gla.ac.uk>
2022-01-12 21:36:51 +0000

    fixed conflicts

--- home/urls.py
+++ home/urls.py
@@ -8,11 +8,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login, name="login"),
     path('signup/', views.signup, name="signup"),
-    path('myprofile/', views.myprofile, name="myprofile"),
-    path('<int:id>/', views.magazine, name="magazine"),
     path('login/', views.user_login, name="login"),
     path('signup/', views.user_signup, name="signup"),
     path('<int:id>', views.magazine, name="magazine"),
     path('<int:id>/<slug:slug>/', views.issue, name="issue"),
-    path('signout/', views.user_signout, name="signout"),
 ]
