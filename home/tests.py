f2e86fb50f0254306d7c21b2934200d907c9a8b1
JanWieczorek-dev <76444737+JanWieczorek-dev@users.noreply.github.com>
2022-02-06 23:13:57 +0000

    a few tests - tbc

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+
+
+class URLTestsNotLoggedIn(TestCase):
+    def test_homapage_url(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_loginpage(self):
+        response = self.client.post(reverse('home:login'), {'username': 'john', 'password': 'password'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_contactpage(self):
+        response = self.client.get(reverse('home:contact'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_membership(self):
+        response = self.client.get(reverse('home:membership'))
+        self.assertEqual(response.status_code, 200)
+
+class URLTestsLoggedIn(TestCase):
+
+    def test_myprofile(self):
+        self.client.login(username='john', password='password')
+
