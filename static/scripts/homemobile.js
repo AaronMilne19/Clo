ac787e746b3b648059ddee7a0f42a714afe5ede8
JanWieczorek-dev <76444737+JanWieczorek-dev@users.noreply.github.com>
2022-02-17 12:51:24 +0000

    JS logic of the mobile "accordion" menu improved

--- static/scripts/homemobile.js
+++ static/scripts/homemobile.js
@@ -1,21 +1,30 @@
 var acc = document.getElementsByClassName("menuelement");
 var about = document.getElementById("aboutclo")
 var openedElement = null;
+about.style.maxHeight = about.scrollHeight + 'px';
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-      about.style.maxHeight = about.scrollHeight + "px";
+    var thisPanel = this.nextElementSibling;
+    if (openedElement == this) {
+      thisPanel.style.maxHeight = null;
+      about.style.maxHeight = about.scrollHeight + 'px';
+      openedElement = null;
+    }
 
+    else if (openedElement) {
+      var openedPanel = openedElement.nextElementSibling;
+      openedPanel.style.maxHeight = null;
+      thisPanel.style.maxHeight = thisPanel.scrollHeight + "px";
+      openedElement = this;
+    }
 
-
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+    else {
+      thisPanel.style.maxHeight = thisPanel.scrollHeight + "px";
       about.style.maxHeight = 0;
+      openedElement = this;
 
 
     }
