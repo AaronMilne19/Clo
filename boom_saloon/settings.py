d122655689db8ea326e8700c80ef9205b0b58a84
AaronMilne19 <77485624+AaronMilne19@users.noreply.github.com>
2022-03-16 20:34:49 +0000

    Update settings.py

--- boom_saloon/settings.py
+++ boom_saloon/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'clo.magazines@gmail.com'
-EMAIL_HOST_PASSWORD = 'CloMagazines2022'
+#EMAIL_HOST_PASSWORD = <REDACTED>
 EMAIL_USE_TLS = True
