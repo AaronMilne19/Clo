c004097f6f119269fd2bde39eb7cb15c37dc72ad
Aaron Milne <2466574M@student.gla.ac.uk>
2022-03-17 15:15:55 +0000

    changed STATIC_ROOT to make admin panel css work on pythonanywhere

--- boom_saloon/settings.py
+++ boom_saloon/settings.py
@@ -134,7 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_ROOT = STATIC_DIR
+#STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Media files
 
